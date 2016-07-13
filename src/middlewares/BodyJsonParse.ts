76b08af09da1f9b7b8b5e87e528937d52d973c5f
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2016-07-13 23:35:52 +0200

    doc: bodyjsonparse comments

--- src/middlewares/BodyJsonParse.ts
+++ src/middlewares/BodyJsonParse.ts
@@ -4,7 +4,23 @@ import {iMiddleware} from './iMiddleware';
 import {HTTP_METHODS} from '../Route';
 import * as querystring from 'querystring';
 
+/**
+ * Parse a http-request body to JSON
+ * 
+ * @export
+ * @class BodyJsonParse
+ * @implements {iMiddleware}
+ */
 export class BodyJsonParse implements iMiddleware {
+    /**
+     * The middleware function doing the parsing of body data.
+     * The content type of a request is checked for what type of parsing is required
+     * eiter using plain JSON.parsing, or querystring
+     * 
+     * @param {iBodyRequest} req the http-request object
+     * @param {http.ServerResponse} res the http-response object
+     * @returns {boolean} whether the parsing of body data succeeded or not
+     */
     public alter(req: iBodyRequest, res: http.ServerResponse): boolean {
 
         if (req.contentType === 'application/x-www-form-urlencode') { // Body is in url form (using & and = for denoting key-value pairs)

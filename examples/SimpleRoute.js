42a0e04446e7bb900b0d813a1981e7b698c95f73
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2017-04-28 15:00:44 +0200

    chore: simple example

---
+++ examples/SimpleRoute.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var fuhttp = require('../');
+
+var route = new fuhttp.Route('api');
+route.get("hello/:name", function (req, res, name) {
+    console.log("GET: Hello " + name);
+    res.write("hello " + name);
+    res.end();
+});
+
+route.post("hello/:name", function (req, res, name) {
+    console.log("POST: Hello " + name);
+    res.write("hello " + name);
+    res.end();
+});
+
+route.put("hello/:name", function (req, res, name) {
+    console.log("PUT: Hello " + name);
+    res.write("hello " + name);
+    res.end();
+});
+
+route.delete("hello/:name", function (req, res, name) {
+    console.log("DELETE: Hello " + name);
+    res.write("hello " + name);
+    res.end();
+});
+
+route.get('ok', function (req, res) {
+    console.log("OK?");
+    res.write("OKEY???");
+    res.end();
+});
+
+var server = new fuhttp.Server(5000);
+server.addRoute(route);
+
+server.listen();

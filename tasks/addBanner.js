1d2e42bea34afdc2814db6a875f66905ad770dae
Jonas Kirkemyr <jonas.kirkemyr@em1.no>
2018-07-04 22:02:17 +0200

    fix: Reference to banner

--- tasks/addBanner.js
+++ tasks/addBanner.js
@@ -7,24 +7,21 @@ function appendBanner(directory) {
     let files = fs.readdirSync(directory);
 
     for (let i = 0; i < files.length; ++i) {
-
         let stat = fs.statSync(directory + files[i]);
 
         if (stat.isDirectory()) {
             let subDirectory = directory + files[i] + '/';
             appendBanner(subDirectory);
-        }
-        else if (path.extname(files[i]) === '.js') {
+        } else if (path.extname(files[i]) === '.js') {
             let options = {
-                banner: 'tasks/banner.tmpl',
+                banner: './tasks/banner.tmpl',
                 filename: files[i]
             };
             let newFile = banner(directory + files[i], options);
 
             fs.writeFileSync(directory + files[i], newFile);
             console.log('\t' + files[i]);
-        }
-        else {
+        } else {
             console.log('\tIgnoring ' + files[i]);
         }
     }
@@ -34,8 +31,3 @@ let files = fs.readdirSync(buildFolder);
 
 console.log('Adding banners to files: ');
 appendBanner(buildFolder, files);
-
-
-
-
-
